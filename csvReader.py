76f12f0f8eef7aa41c87cd4a88b127d33b927e27
Kevin <kevin.kochiss21@gmail.com>
2021-07-11 21:12:39 -0700

    Test

--- csvReader.py
+++ csvReader.py
@@ -2,5 +2,10 @@ import csv
 file_path   = ''
 file_name    = ''
 
+## Main loop
 with open(file_name, 'r') as csvfile:
     csvreader = csv.reader(csvfile)
+    #### read rows
+    #### write them to a sepreate csv label with with a batch number
+    #### 
+    string = 'test'
