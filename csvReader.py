45ceb3cda1d8a7835365988b15cfe1bfb93d5401
Kevin <kevin.kochiss21@gmail.com>
2021-07-11 21:45:07 -0700

    File Path

--- csvReader.py
+++ csvReader.py
@@ -1,11 +1,23 @@
 import csv
-file_path   = ''
-file_name    = ''
+from pathlib import Path
+
 
 ## Main loop
-with open(file_name, 'r') as csvfile:
+#Access the manifest log to see what file should be accessed, ie if a new file has been created
+my_file_path = Path(r'C:\Users\kevin\Documents\graphtec\GL100_240_840-APS\Data\2021-07-11.csv')
+print(my_file_path)
+with open(my_file_path, 'r') as csvfile:
+    print('Hello')
     csvreader = csv.reader(csvfile)
     #### read rows
+    #fields = next(csvreader)
+    row_num = 0
+    for row in csvreader:
+        row_num += 1
+        #print("Row {}: {}")
+        for col in row:
+            print("Row {}: ".format(row_num))
+            print("%10s"%col),
+            print('\n')
     #### write them to a sepreate csv label with with a batch number
-    #### 
-    string = 'test'
+    
