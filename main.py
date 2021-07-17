f22648077ae975589e6e209df7b649aaf89466fa
Kevin-Kochiss <69224072+Kevin-Kochiss@users.noreply.github.com>
2021-07-17 11:22:37 -0700

    Initial scan rate update, now updates every 5 seconds unless told to run

--- main.py
+++ main.py
@@ -5,9 +5,13 @@ from configuration import ScriptVars, initialize
 initialize()
 
 while True:
-    config = ScriptVars()
-    root_dir = config.config['ROOT_DIR']
-    scan_rate = config.config['SCAN_RATE']
+    config      = ScriptVars()
+    root_dir    = config.config['ROOT_DIR']
+
     if config.can_run():
+        scan_rate = config.config['SCAN_RATE']
         scan_files(root_dir)
+    else:
+        scan_rate   = 5
+
     time.sleep(scan_rate)
